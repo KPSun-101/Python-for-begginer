22cbded49567001c12f6a7bca70887bd6e1b0974
Md Kamal Parvez Sun <kamal.prstucse1.39@gmail.com>
2026-07-01 19:57:24 +0600

    Create 2.Type casting in Python.cpp

---
+++ 2.Type casting in Python.cpp
@@ -0,0 +1,16 @@
+# If we want to take input as integer,float etc then,
+
+A=int(input())
+B=int(input()) #now it converts our input to integer
+
+print(A+B) # If A=5,B=6 then A+B=11. Same method for others
+
+# More information
+
+X=float(34) #Now X=34.0
+Y=int(3.1416) #Now Y=3
+Z=str(123) #Now Z is a string
+
+print(type(Z)) # <class 'str'>  type() is used to know data type
+
+print("X=",X," Y=",Y) # Here X= 34.0 and Y=3
