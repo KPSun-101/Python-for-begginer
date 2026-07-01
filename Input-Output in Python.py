7a6694834d10e10fc124ab4da0e7a47e3b359aa3
Md Kamal Parvez Sun <kamal.prstucse1.39@gmail.com>
2026-07-01 19:37:35 +0600

    Create Input-Output in Python.py

---
+++ Input-Output in Python.py
@@ -0,0 +1,10 @@
+print("Hello World!") #output system in python
+
+Name=input("Enter Your Name:") #Input system in python
+print("My name is ",Name)
+
+# Python takes all input as a string 
+
+A=input()
+B=input()
+print(A+B) # if A=5,B=6 then now it gives A+B=56
