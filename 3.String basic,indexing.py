908e0d94a7f94bfe324d64de875275effc5f7c03
Md Kamal Parvez Sun <kamal.prstucse1.39@gmail.com>
2026-07-01 20:46:02 +0600

    Create 3.String basic,indexing.py

---
+++ 3.String basic,indexing.py
@@ -0,0 +1,13 @@
+Name="Kamal Parvez Sun" 
+print(Name) # It prints Kamal Parvez Sun
+
+Info="""I am Kamal
+I am a student"""
+print(Info) # Print in given format
+
+# String Sliching
+
+print(Name[1:8]) #Print char from index 1 to 8(not included)
+print(Name[:4]) #print char from 0 to 4(not included)
+print(Name[2:]) #Print char from 2(included) to end
+print(Name[-5:-1]) #Print  from -5(not inc) to -1(included)
