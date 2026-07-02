763f5484b4b77e1a8958a3ed9f6ddcb54c6da255
Md Kamal Parvez Sun <kamal.prstucse1.39@gmail.com>
2026-07-02 16:31:03 +0600

    Create 5.String last.py

---
+++ 5.String last.py
@@ -0,0 +1,18 @@
+# Concatenation
+A="My name is"
+B="James"
+print(A+" "+B) #output My name is James
+
+value=100
+text=f"My value is {value}" 
+print(text) #output  My value is 100
+
+# If we don't give f then it prints My value is {value}
+
+text2="My value is {value}"
+print(text2) #output  My value is {value}
+
+# Important link for extra info about string in Python
+https://www.w3schools.com/python/python_strings_escape.asp
+# Details about String methods in Pyhton
+https://www.w3schools.com/python/python_strings_methods.asp
