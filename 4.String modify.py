1dbeac58cb09886d3a6e8ac7c41fc2488e516ec6
Md Kamal Parvez Sun <kamal.prstucse1.39@gmail.com>
2026-07-02 16:16:38 +0600

    Create 4.String modify.py

---
+++ 4.String modify.py
@@ -0,0 +1,16 @@
+#The upper() method returns the string in upper case
+Name="james TOm"
+print(Name.upper()) # JAMES TOM
+
+#The lower() method returns the string in lower case
+print(Name.lower()) #james tom
+
+#The strip()method removes any whitespace from the beginning or the end
+City=" Dhaka Pirojpur "
+print(City.strip()) # Dhaka Pirojpur
+
+#The replace() method replaces a string with another string
+print(Name.replace("K","J")) # james TOm
+
+#The split()method splits the string into substrings if it finds instances of the separator
+print(Name.split()) # ['james', 'TOm']
